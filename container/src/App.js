a761bb925f8188d13a6eb0c6dff1ce0178c53b89
Guille <guillermo.paniaguasilva@MacBookProM2Max.local>
2023-12-31 15:04:19 -0300

    add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
